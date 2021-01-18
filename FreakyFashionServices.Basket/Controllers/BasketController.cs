cc4522e5b5964512da01a975b7d081bddd950778
tariq <tariq@LAPTOP-50PAEQKI>
2021-01-18 16:38:27 +0100

    completed 1st inlamning

---
+++ FreakyFashionServices.Basket/Controllers/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using FreakyFashionServices.Basket.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FreakyFashionServices.Basket.Extensions;
+
+namespace FreakyFashionServices.Basket.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BasketController : ControllerBase
+    {
+        //List<string> todos = new List<string>(); 
+        private readonly IDistributedCache cache;
+        public BasketController(IDistributedCache cache)
+        {
+            this.cache = cache;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<CreateBasketDto> GetBasket(String id)
+        {
+            var serializedData = await cache.GetStringAsync(id);
+            var basket = JsonSerializer.Deserialize<CreateBasketDto>(serializedData);
+            
+            return basket; //200 ok
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBasket(CreateBasketDto createBasketDto)
+        {
+            await cache.SetRecordAsync(createBasketDto.Id, createBasketDto);           
+            return Ok(); //200 ok
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Putbas(int id, [FromBody] CreateBasketDto basket)
+        {
+            await cache.SetRecordAsync<CreateBasketDto>(id.ToString(), basket);
+            return Ok();
+        }
+
+
+        
+    }
+}
--- FreakyFashionServices.Basket/Startup.cs
+++ FreakyFashionServices.Basket/Startup.cs
@@ -1,19 +1,18 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using FreakyFashionServices.Catalog.Models;
-using FreakyFashionServices.Catalog.Models.Data;
-using Microsoft.EntityFrameworkCore;
 
-namespace FreakyFashionServices.Catalog
+namespace FreakyFashionServices.Basket
 {
     public class Startup
     {
@@ -27,20 +26,18 @@ namespace FreakyFashionServices.Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var server = Configuration["DBServer"] ?? "localhost";
-            var port = Configuration["DBPort"] ?? "1433";
-            var user = Configuration["DBUser"] ?? "SA";
-            var password = Configuration["DBPassword"] ?? "Testing1122";
-            var database = Configuration["Database"] ?? "SystemIntegDocker";
-
-            services.AddDbContext<FreakyFashionContext>(options =>
-                options.UseSqlServer($"Server=ms-sql-server;Initial Catalog={database}; User ID={user}; Password={password}"));
-
-            //services.AddDbContext<FreakyFashionContext>(options =>
-            //    options.UseSqlServer(
-            //        Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreakyFashionServices.Basket", Version = "v1" });
+            });
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = Configuration.GetConnectionString("Redis");
+                options.InstanceName = "Basket_";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,11 +46,13 @@ namespace FreakyFashionServices.Catalog
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "FreakyFashionServices.Basket v1"));
             }
 
-            app.UseRouting();
+            app.UseHttpsRedirection();
 
-            PrepDB.PrepPopulation(app);
+            app.UseRouting();
 
             app.UseAuthorization();
 
